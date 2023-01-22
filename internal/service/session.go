bccfb248462684fb28cf4da56e382c75b0b9c8a0
with-insomnia <tanatar-abdurahman@mail.ru>
2023-01-22 22:32:16 +0600

    add services

--- internal/service/session.go
+++ internal/service/session.go
@@ -1,1 +1,40 @@
 package service
+
+import (
+	"github.com/with-insomnia/Forum-Golang/internal/model"
+	"github.com/with-insomnia/Forum-Golang/internal/repository"
+)
+
+type SessionService interface {
+	CreateSession(session model.Session) error
+	GetSessionByToken(token string) (model.Session, error)
+	DeleteSession(token string) error
+}
+
+type sessionService struct {
+	repository.SessionQuery
+}
+
+func NewSessionService(dao repository.DAO) SessionService {
+	return &sessionService{dao.NewSessionQuery()}
+}
+
+func (s *sessionService) CreateSession(session model.Session) error {
+	err := s.SessionQuery.CreateSession(session)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *sessionService) GetSessionByToken(token string) (model.Session, error) {
+	session, err := s.SessionQuery.GetSessionByToken(token)
+	if err != nil {
+		return model.Session{}, err
+	}
+	return session, nil
+}
+
+func (s *sessionService) DeleteSession(token string) error {
+	return s.SessionQuery.DeleteSession(token)
+}
--- internal/service/user.go
+++ internal/service/user.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"github.com/with-insomnia/Forum-Golang/internal/model"
+	"github.com/with-insomnia/Forum-Golang/internal/repository"
+)
+
+type UserService interface {
+	GetUserByToken(token string) (*model.User, error)
+}
+
+type userService struct {
+	repository repository.UserQuery
+}
+
+func NewUserService(dao repository.DAO) UserService {
+	return &userService{
+		dao.NewUserQuery(),
+	}
+}
+
+func (u *userService) GetUserByToken(token string) (*model.User, error) {
+	userID, err := u.repository.GetUserIdByToken(token)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := u.repository.GetUserByUserId(*userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
