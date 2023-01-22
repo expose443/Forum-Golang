e949c38a977382120e0487506eb325b23fa17cab
with-insomnia <tanatar-abdurahman@mail.ru>
2023-01-22 22:32:44 +0600

    add two repo

--- internal/repository/session.go
+++ internal/repository/session.go
@@ -1,9 +1,64 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+	"log"
 
-type SessionQuery interface{}
+	"github.com/with-insomnia/Forum-Golang/internal/model"
+)
+
+type SessionQuery interface {
+	GetSessionByToken(token string) (model.Session, error)
+	CreateSession(session model.Session) error
+	DeleteSession(token string) error
+}
 
 type sessionQuery struct {
 	db *sql.DB
+}
+
+func (s *sessionQuery) CreateSession(session model.Session) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("INSERT INTO sessions(email, username, token, expiry) VALUES(?,?,?,?)", session.Email, session.Username, session.Token, session.Expiry)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func (s *sessionQuery) GetSessionByToken(token string) (model.Session, error) {
+	stmt, err := s.db.Prepare("SELECT session_id, email, username, token, expiry FROM sessions WHERE token = ?")
+	if err != nil {
+		log.Println(err)
+	}
+	row := stmt.QueryRow(token)
+	var session model.Session
+	if err := row.Scan(&session.ID, &session.Email, &session.Username, &session.Token, &session.Expiry); err != nil {
+		return model.Session{}, err
+	}
+	return session, nil
+}
+
+func (s *sessionQuery) DeleteSession(token string) error {
+	stmt, err := s.db.Prepare("DELETE FROM sessions WHERE token = ?")
+	if err != nil {
+		return err
+	}
+	res, err := stmt.Exec(token)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return errors.New("delete session was failed")
+	}
+	return nil
 }
--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,9 +1,43 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
 
-type UserQuery interface{}
+	"github.com/with-insomnia/Forum-Golang/internal/model"
+)
+
+type UserQuery interface {
+	GetUserIdByToken(token string) (*int, error)
+	GetUserByUserId(userID int) (*model.User, error)
+}
 
 type userQuery struct {
 	db *sql.DB
+}
+
+func (u *userQuery) GetUserIdByToken(token string) (*int, error) {
+	stmt, err := u.db.Prepare("SELECT user_id FROM sessions WHERE token=?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	row := stmt.QueryRow(token)
+	var userID int
+	if err := row.Scan(&userID); err != nil {
+		return nil, nil
+	}
+	return &userID, nil
+}
+
+func (u *userQuery) GetUserByUserId(userID int) (*model.User, error) {
+	stmt, err := u.db.Prepare("SELECT * FROM users WHERE user_id = ?")
+	if err != nil {
+		return nil, err
+	}
+	row := stmt.QueryRow(userID)
+	var user model.User
+	if err := row.Scan(&user.ID, &user.Email, &user.Password, &user.Username); err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
