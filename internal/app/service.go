6918bacc0fe3acabef3a39f3d47229d92c4e15ca
with-insomnia <tanatar-abdurahman@mail.ru>
2023-01-22 22:31:48 +0600

    add 2 services

--- internal/app/service.go
+++ internal/app/service.go
@@ -1,3 +1,16 @@
 package app
 
-type App struct{}
+import "github.com/with-insomnia/Forum-Golang/internal/service"
+
+type App struct {
+	sessionService service.SessionService
+	userService    service.UserService
+}
+
+func NewAppService(
+	sessionService service.SessionService,
+) App {
+	return App{
+		sessionService: sessionService,
+	}
+}
